c4e908291ce91301a481ee29f6fa7bc416dc1ce9
JayJhaveri1906 <jjhaveri1906@gmail.com>
2023-12-13 01:09:18 -0800

    Added cache in Python server
    Co-authored-by: Xuanyu <1292557179@qq.com>
    Co-authored-by: Niraj <nirajyagnik80@gmail.com>

--- AltBot_Chrome_Extension_Mastadon/ml_api/demo_ML_Api.py
+++ AltBot_Chrome_Extension_Mastadon/ml_api/demo_ML_Api.py
@@ -3,40 +3,74 @@ import requests
 import json
 import time
 from flask_cors import CORS
+import pickle
+import os
 
 app = Flask(__name__)
 CORS(app)
+auth = HTTPBasicAuth()
+pickle_filename = "cache_dict.pickle"
+
+# Add your username and password
+USERNAME = "your_username"
+PASSWORD = "your_password"
+
+
+@auth.verify_password
+def verify_password(username, password):
+    return username == USERNAME and password == PASSWORD
 
 
 @app.route('/', methods=['GET', 'POST'])
+@auth.login_required
 def handle_request():
-    url = str(request.args.get('input'))
+    if os.path.exists(pickle_filename):
+        with open(pickle_filename, 'rb') as file:
+            cache_dict = pickle.load(file)
+    else:
+        cache_dict = {}
+        with open(pickle_filename, 'wb') as file:
+            pickle.dump(cache_dict, file)
+
+    try:
+        data = request.json
+        url = data.get('input')
+        if not url:
+            raise ValueError('Missing input URL')
+
+        if url in cache_dict:
+            result = {'text': cache_dict[url], 'timestamp': time.time()}
+            return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': f'Error processing: {str(e)}'}), 400
+    
     API_TOKEN = "Insert_Token_Here"
     model = 'nlpconnect/vit-gpt2-image-captioning'
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
-    data = {"inputs": url}
 
     try:
         # Try to send a request to the API endpoint
         response = requests.post(
             f'https://api-inference.huggingface.co/models/{model}',
             headers=headers,
-            json=data
+            json={"inputs": url}
         )
+        response.raise_for_status()
     except Exception as e:
-        return ("error: Error processing %s" % (str(e)))
-
+        return jsonify({'error': f'Error getting response from endpoint: {str(e)}'}), 500
+    
     try:
         alt_text = response.json()[0]['generated_text']
-        # alt_text = response.json()
     except Exception as e:
-        return ("error: Error processing %s" % (str(e)))
+        return jsonify({'error': f'Error subtracting text: {str(e)}'}), 500
+    
+    result = {'text': alt_text, 'timestamp': time.time()}
+    cache_dict[url] = alt_text
 
-    r = {'text': alt_text, 'timestamp': time.time()}
-    json_dump = json.dumps(r)
-
-    return json_dump
+    with open(pickle_filename, 'wb') as file:
+        pickle.dump(cache_dict, file)
+        
+    return jsonify(result)
 
 
 if __name__ == "__main__":
