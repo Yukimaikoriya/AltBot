f58aafd73bed6b147bfd055e214f4ab813995889
Yukimai <yukimaikoriya@vip.qq.com>
2023-12-13 11:04:18 -0800

    Update jest.config.js

--- AltBot_Chrome_Extension_Mastadon/foreground/jest.config.js
+++ AltBot_Chrome_Extension_Mastadon/foreground/jest.config.js
@@ -155,7 +155,7 @@ const config = {
 
   // The glob patterns Jest uses to detect test files
   // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/test/**/*.[jt]s?(x)",
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
@@ -165,7 +165,7 @@ const config = {
   // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
-  // testRegex: [],
+  testRegex: ['test/.+\\.test\\.js'],
 
   // This option allows the use of a custom results processor
   // testResultsProcessor: undefined,
